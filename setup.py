49cd215a74d7caf91729a87867131ac6d624480f
daveoncode <davidezanotti@gmail.com>
2015-07-30 16:13:10 +0200

    version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-string-utils',
-    version='0.0.0',
+    version='0.1.0',
     description='Utility functions for strings checking and manipulation.',
     author='Davide Zanotti',
     author_email='davidezanotti@gmail.com',
