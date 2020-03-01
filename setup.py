26016e1216a304742b6f47fedc03f9bf9418ad73
Davide Zanotti <davidezanotti@gmail.com>
2020-03-01 18:27:55 +0100

    updated setup.py (using setuptools) + added build in travis config

--- setup.py
+++ setup.py
@@ -1,40 +1,28 @@
-from distutils.core import setup
+import setuptools
 
 with open('README.md', 'r') as readme:
     long_description = readme.read()
 
-setup(
+setuptools.setup(
     name='python-string-utils',
-    version='0.6.0',
-    description='Utility functions for strings checking and manipulation.',
+    version='1.0.0',
+    description='Utility functions for strings validation and manipulation.',
     long_description=long_description,
     author='Davide Zanotti',
     author_email='davidezanotti@gmail.com',
     license='MIT',
     url='https://github.com/daveoncode/python-string-utils',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
+        'Programming Language :: Python :: 3',
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
-
-        # Pick your license as you wish (should match "license" above)
+        'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
-    keywords='string str utilities development',
-    py_modules=['string_utils'],
+    keywords='string str utilities validation compression development',
+    packages=['string_utils'],
     data_files=[('README', ['README.md'])],
+    python_requires='>=3.5',
+    setup_requires=['wheel'],
 )
