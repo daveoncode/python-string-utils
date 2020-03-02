33ddf0998d74867418a1ee24e05c820a6953b64d
Davide Zanotti <davidezanotti@gmail.com>
2020-03-02 17:35:18 +0100

    updates readthedocs config, removed docs build dir

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '.ENV', '.venv']
+exclude_patterns = ['build', '.venv', '.tox', 'htmlcov']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
